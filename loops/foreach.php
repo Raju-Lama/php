c74e7bb12b14b2d194acb0f2eb3c1b34e83b494d
rajukaji <rajulamarainon@gmail.com>
2025-07-23 11:13:59 +0545

    substring php

---
+++ loops/foreach.php
@@ -0,0 +1,7 @@
+<?php
+// Looping through an array using foreach
+$fruits = ["Apple", "Banana", "Cherry", "Date"];
+foreach ($fruits as $fruit) {
+    echo "I like $fruit <br>";
+}
+?>
---
+++ strings/substring.php
@@ -0,0 +1,7 @@
+<?php
+    $word = "Kathmandu";
+    echo substr($word, 4, 5);
+    echo $word[4];
+    // echo $word{5};
+    // deprecated in PHP 7.4, use $word[5] instead
+?>
